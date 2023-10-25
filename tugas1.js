cedabb3da570d63c5e2e21eb9d5c089644b864fc
putragabrielll <putragmahk@gmail.com>
2023-10-25 20:07:03 +0700

    tugas 2 update

--- tugas1.js
+++ tugas1.js
@@ -28,13 +28,13 @@ console.log(contoh4)
 // 5. Includes
 // Digunakan untuk MENGECEK apakah dalam sebuah Array terdapa element tertentu, dan '0' itu opsional mau mulai dari index keberapa akan di mulai pengecekan, tapi default nya adalah '0'
 const contoh5 = ["gabriel", "putra", "advent", "yakop", "sihombing"];
-let dataBaru5 = contoh5.includes('yakop', 1)
+let dataBaru5 = contoh5.includes('yakop', 0)
 console.log(dataBaru5)
 4
 // 6. IndexOf
 // Untuk mencari letak dari sebuah element pada Array
 const contoh6 = ["gabriel", "putra", "advent", "yakop", "sihombing"];
-let dataBaru6 = contoh6.indexOf('yakop', 4)
+let dataBaru6 = contoh6.indexOf('yakop', 0)
 console.log(dataBaru6)
 
 // 7. Concat
