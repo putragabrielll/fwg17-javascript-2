b6296f8f5a65b818c61d2aa899d2439c4aef6143
putragabrielll <putragmahk@gmail.com>
2023-10-25 19:13:30 +0700

    tugas 2 update

--- tugas2.js
+++ tugas2.js
@@ -1,1 +1,28 @@
-const name = ['Abigail', 'Alexandra', 'Alison', 'Amanda', 'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn','Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 'Olivia', 'Penelope']
+const name = [
+  "Abigail",
+  "Alexandra",
+  "Alison",
+  "Amanda",
+  "Angela",
+  "Bella",
+  "Carol",
+  "Caroline",
+  "Carolyn",
+  "Deirdre",
+  "Diana",
+  "Elizabeth",
+  "Ella",
+  "Faith",
+  "Olivia",
+  "Penelope",
+];
+
+function sarchName(search, jumlah, callback) {
+  let data = name.filter((b) => b.toLowerCase().includes(search));
+  return callback(data, jumlah);
+}
+function Mencari(data, jumlah) {
+  data = data.slice(0, jumlah);
+  return data;
+}
+console.log(sarchName("an", 3, Mencari));
