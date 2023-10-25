5c4bc56a6eede0e15035ee437269f3570daefc1c
putragabrielll <putragmahk@gmail.com>
2023-10-25 19:12:24 +0700

    tugas 3 update

---
+++ tugas3.js
@@ -0,0 +1,10 @@
+const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
+    if (nilaiAwal < nilaiAkhir) {
+        if (dataArray.length > 5) {
+            
+        }
+    }
+
+}
+
+seleksiNilai(2, 20, [2, 25, 4, 14, 17, 30, 8]);
