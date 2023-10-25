d583f037118b5c55b0934132c6e2506b79a95720
putragabrielll <putragmahk@gmail.com>
2023-10-25 19:13:48 +0700

    tugas 2 update

--- tugas2.js
+++ tugas2.js
@@ -17,12 +17,4 @@ const name = [
   "Penelope",
 ];
 
-function sarchName(search, jumlah, callback) {
-  let data = name.filter((b) => b.toLowerCase().includes(search));
-  return callback(data, jumlah);
-}
-function Mencari(data, jumlah) {
-  data = data.slice(0, jumlah);
-  return data;
-}
-console.log(sarchName("an", 3, Mencari));
+
