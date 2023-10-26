0fd6571b293929c26cdb5786ab8026a20b78b3d0
putragabrielll <putragmahk@gmail.com>
2023-10-26 18:01:24 +0700

    tugas 2 update

--- tugas2.js
+++ tugas2.js
@@ -1,4 +1,4 @@
-const name = [
+const peserta = [
   "Abigail",
   "Alexandra",
   "Alison",
@@ -16,5 +16,56 @@ const name = [
   "Olivia",
   "Penelope",
 ];
+const convertToLowerCase = (nama) => {
+  const cekLowerCase = {
+    A: "a",
+    B: "b",
+    C: "c",
+    D: "d",
+    E: "e",
+    F: "f",
+    G: "g",
+    H: "h",
+    I: "i",
+    J: "j",
+    K: "k",
+    L: "l",
+    M: "m",
+    N: "n",
+    O: "o",
+    P: "p",
+    Q: "q",
+    R: "r",
+    S: "s",
+    T: "t",
+    U: "u",
+    V: "v",
+    W: "w",
+    X: "x",
+    Y: "y",
+    Z: "z",
+  };
 
+  // temp = g
+  let temp = "";
+  for (let i = 0; i < nama.length; i++) {
+    console.log(nama[i]);
+    console.log(cekLowerCase[nama[i]]);
+    if (cekLowerCase[nama[i]] != undefined) {
+      temp += cekLowerCase[nama[i]];
+    } else {
+      temp += nama[i];
+    }
+  }
+  return temp;
+};
 
+console.log(peserta.length)
+const cekNama = () => {
+  for (let i = 0; i < peserta.length; i++){
+    let temp = convertToLowerCase(peserta[i])
+    console.log(temp)
+  }
+}
+
+cekNama()
